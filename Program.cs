2762ea50737f473ed0c266e7274ee892347f8507
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 17:11:41 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("--- Minhas frutas preferidas ---\n");
+        Console.WriteLine("1. Banana");
+        Console.WriteLine("2. Tomate");
+        Console.WriteLine("3. Maçã");
+        Console.WriteLine("4. Mamão");
+        Console.WriteLine("5. Uva");
+    }
+}
